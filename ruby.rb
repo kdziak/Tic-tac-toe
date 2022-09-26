e429c71196f7b471078d6088b067c7feee822b34
Kian Dziak <kiandzk@gmail.com>
2022-09-26 14:06:21 -0500

    Game now checks for wins and draws. Declares winner or draw and ends game as needed. Need to refactor code and clean it up. Happy with my first oop attempt! Lots to learn and improve!

--- ruby.rb
+++ ruby.rb
@@ -23,7 +23,12 @@ end
 
 # class for game loop
 class TikTacToe
+  WINNING = [ [0,1,2],[3,4,5],[6,7,8], #Horizontal
+    [0,3,6],[1,4,7],[2,5,8], #Vertical
+    [0,4,8],[2,4,6]] #Diagonal
+
   def initialize
+    @turn_count = 0
     @board = (1..9).to_a
     @p1 = Player.new(1)
     @p2 = Player.new(2)
@@ -50,6 +55,7 @@ class TikTacToe
   def play
     player = @p1
     available_postions = (1..9).to_a
+    turn_number = 0
 
     until @game_over
       display_board
@@ -60,13 +66,36 @@ class TikTacToe
         p 'Please enter a vaild move'
         player_choice = gets.chomp.to_i
       end
+      turn_number += 1
       player_move(player, player_choice, available_postions)
+      if check_winner(available_postions, player)
+        display_board
+        puts "You won!"
+        @game_over = true
+      elsif
+        check_stalemate(turn_number)
+      end
       player == @p1 ? player = @p2 : player = @p1
     end
+    puts 'game over'
   end
 
   def valid_move(player_choice, player, available_postions)
     available_postions.include?(player_choice)
+  end
+
+  def check_winner(available_postions, player)
+    WINNING.any? do |combo|
+      combo.all? { |number| available_postions[number] == player.piece }
+    end
+  end
+
+  def check_stalemate(turn_number)
+    @game_over = true if turn_number >= 9
+    if @game_over
+      display_board
+      p 'The game is draw'
+    end
   end
 
   def player_move(player, player_choice, available_postions)
